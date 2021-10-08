900e091bd0338f1c151db5e83c4264f7a749616d
Matt Gacy <matt@mgacy.com>
2021-10-08 07:07:26 -0700

    Add TimerRequestView

---
+++ BlueTriangleDemo/Views/TimerRequestView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerRequestView.swift
+//  BlueTriangleDemo
+//
+//  Created by Mathew Gacy on 10/7/21.
+//
+
+import SwiftUI
+
+struct TimerRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerRequestView()
+    }
+}
