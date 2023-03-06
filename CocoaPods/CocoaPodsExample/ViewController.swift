fcc9bc966a95bac44943ac85c6f950e886a59dcd
Matt Gacy <matt@mgacy.com>
2023-03-06 10:46:34 -0700

    Replace SwiftUI app with UIKit one

---
+++ CocoaPods/CocoaPodsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoaPodsExample
+//
+//  Created by Mathew Gacy on 3/6/23.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
