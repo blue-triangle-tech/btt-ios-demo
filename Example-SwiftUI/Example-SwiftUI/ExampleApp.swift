9865b0db3a6628f563d3092157872c3995d25975
jp-aloha <jaib@alohatechnology.com>
2023-06-27 14:33:24 +0530

    Added Cofiguration screen for both target

--- Example-SwiftUI/Example-SwiftUI/ExampleApp.swift
+++ Example-SwiftUI/Example-SwiftUI/ExampleApp.swift
@@ -11,24 +11,9 @@ import SwiftUI
 
 @main
 struct Example_SwiftUIApp: App {
-    init() {
-        
-        // Configure BlueTriangle
-        BlueTriangle.configure { config in
-            config.siteID = "bluetrianglesdk3280971z"
-            config.networkSampleRate = 1.0
-            config.enableScreenTracking = true
-            config.enableDebugLogging = true
-            config.ANRMonitoring = true
-        }
-       // BlueTriangle.metrics?["example-app"] = "custom1"
-    }
-
     var body: some Scene {
         WindowGroup {
-            TabContainerView(
-                imageLoader: .live,
-                service: .captured)
+            BTTRootContrainerView(vm: BTTConfigModel())
         }
     }
 }
---
+++ Example-SwiftUI/Example-UIKit/AppCoordinator.swift
@@ -0,0 +1,54 @@
+//
+//  AppCoordinator.swift
+//
+//  Created by Ashok Singh on 27/06/23.
+//  Copyright © 2023 Blue Triangle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator{
+    
+    static func setupRootTabVc(){
+        if let rootVC = self.getRootTabVC(){
+            switchRoot(sourceVC: rootVC)
+        }
+    }
+    
+    static func setupRootConfigVc(){
+        if let rootVC = self.getRootConfigVC(){
+            switchRoot(sourceVC: rootVC)
+        }
+    }
+    
+    static private func switchRoot(sourceVC: UIViewController, completion: ((Bool) -> Void)? = nil){
+        
+        // Use a UIHostingController as window root view controller.
+        let scene = UIApplication.shared.connectedScenes.first
+        if let sceneDelegate = scene?.delegate as? SceneDelegate, let window  = sceneDelegate.window {
+            UIView.transition(with: window, duration: 0.1, options:[.transitionCrossDissolve, .curveEaseInOut], animations: {
+                window.rootViewController = sourceVC
+                window.makeKeyAndVisible()
+            }, completion: { completed in
+                if let completion = completion {
+                    completion(true)
+                }
+            })
+        }
+    }
+
+    static private func getRootTabVC() ->UITabBarController?{
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = storyboard.instantiateViewController(withIdentifier: "TabBarVC") as? UITabBarController
+        return rootVC
+    }
+    
+    static private func getRootConfigVC() ->UINavigationController?{
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = storyboard.instantiateViewController(withIdentifier: "ConfigVC")
+        let navRootVc = UINavigationController(rootViewController: rootVC)
+        return navRootVc
+    }
+}
+
