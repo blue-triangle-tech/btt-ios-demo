514ac27dc03abad60c63b4752a43ff17da12dd28
JP-aloha <jaib@alohatechnology.com>
2023-07-31 19:31:36 +0530

    Added Some file to Demo cocoa pod test

---
+++ CocoaPods/CocoaPodsExample/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  CocoaPodsExample
+//
+//  Created by Ashok Singh on 31/07/23.
+//  Copyright © 2023 Blue Triangle. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
